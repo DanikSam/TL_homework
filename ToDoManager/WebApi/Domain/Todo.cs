efd7ed9418dbbe82733c17545b04fb084b1b05a1
DanikSam <samokaev_00@mail.ru>
2023-04-24 21:14:48 +0300

    done
    добавлено изменение, удаление, обновление по Id

---
+++ ToDoManager/WebApi/Domain/Todo.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApi.Domain;
+
+public class Todo
+{
+    public int Id { get; init; }
+    public string Title { get; init; }
+    
+    /// <summary>
+    /// День, на который заплонирован Todo
+    /// </summary>
+    public DateTime PlannedDay{ get; set; }
+
+    public Todo(int id, string title, DateTime plannedDay)
+    {
+        Id = id;
+        Title = title;
+        PlannedDay = plannedDay;
+    }
+
+    public Todo(int id, string title)
+    {
+        Id = id;
+        Title = title;
+        PlannedDay = DateTime.UtcNow;
+    }
+}
